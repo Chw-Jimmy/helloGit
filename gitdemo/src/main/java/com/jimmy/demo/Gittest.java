2402347826e03191047c76fbe561c67d1cdc21a8
jimmy <houwang.chen@smr-nbhx.com>
2021-12-05 16:52:49 +0800

    版本修改

---
+++ gitdemo/src/main/java/com/jimmy/demo/Gittest.java
@@ -0,0 +1,17 @@
+package com.jimmy.demo;
+
+/**
+ * @author: 陈后望_Jimmy
+ * @date: 2021年12月05日 15:21:54
+ */
+public class Gittest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello 版本2");
+        System.out.println("hello 版本3");
+        System.out.println("hello 版本4");
+        System.out.println("hello 版本5");
+        System.out.println("hello 版本6");
+
+    }
+}
